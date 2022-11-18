66faf5d92ae7e179b56e3656bb6310e463c4cbc5
Segundo <segundogaldames@gmail.com>
2022-11-18 18:23:09 -0300

    Desactivacion temporal de auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth','active']);
+        //$this->middleware(['auth','active']);
 
     }
     /**
